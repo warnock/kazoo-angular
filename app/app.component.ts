1f7a04599de4430840f695072922f638a301afb8
Megan Warnock and Ryan McAlpin <ryan@>
2017-05-01 14:33:54 -0700

    add lesson boilerplate

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+      <h1>Puppies!</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
